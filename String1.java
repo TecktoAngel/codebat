681ce3f3a95aac62d2549adc961903d6c6fd3302
Evgeniy <zhenya_bal@ua.fm>
2017-02-14 21:50:33 +0200

    New methods in String1

--- String1.java
+++ String1.java
@@ -126,5 +126,203 @@ public class String1 {
         return str.substring(lastTwo) + str.substring(0, lastTwo);
     }
 
+    /*
+    theEnd
+    Given a string, return a string length 1 from its front, unless front is false, in which case
+    return a string length 1 from its back. The string will be non-empty.
+    */
+    public String theEnd(String str, boolean front) {
+        if (!front) {
+            return str.substring(str.length() - 1);
+        }
+        return str.substring(0, 1);
+    }
+
+    /*
+    withouEnd2
+    Given a string, return a version without both the first and last char of the string.
+    The string may be any length, including 0.
+    */
+    public String withouEnd2(String str) {
+        if (str.length() < 1) {
+            return str;
+        } else if (str.length() == 1) {
+            return "";
+        }
+        return str.substring(1, str.length() - 1);
+    }
+
+    /*
+    middleTwo
+    Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri".
+    The string length will be at least 2.
+    */
+    public String middleTwo(String str) {
+        int middle = str.length() / 2;
+
+        return str.substring(middle - 1, middle + 1);
+    }
+
+    /*
+    endsLy
+    Given a string, return true if it ends in "ly".
+    */
+    public boolean endsLy(String str) {
+        if (str.length() >= 2 && str.substring(str.length() - 2).equals("ly")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    nTwice
+    Given a string and an int n, return a string made of the first and last n chars from the string.
+    The string length will be at least n.
+    */
+    public String nTwice(String str, int n) {
+        return str.substring(0, n) + str.substring(str.length() - n);
+    }
+
+    /*
+    twoChar
+    Given a string and an index, return a string length 2 starting at the given index. If the index
+    is too big or too small to define a string length 2, use the first 2 chars. The string length will be at least 2.
+    */
+    public String twoChar(String str, int index) {
+        if (index >= 0 && index <= str.length() - 2) {
+            return str.substring(index, index + 2);
+        } else {
+            return str.substring (0, 2);
+        }
+    }
+
+    /*
+    middleThree
+    Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and".
+    The string length will be at least 3.
+    */
+    public String middleThree(String str) {
+        int middle = str.length() / 2;
+        if (str.length() < 3) {
+            return str;
+        } else {
+            return str.substring(middle - 1, middle + 2);
+        }
+    }
+
+    /*
+    hasBad
+    Given a string, return true if "bad" appears starting at index 0 or 1 in the string, such as with "badxxx" or
+    "xbadxx" but not "xxbadxx". The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+    */
+    public boolean hasBad(String str) {
+        if (str.length() == 3 && str.equals("bad")) {
+            return true;
+        } else if (str.length() >= 4 && (str.substring(0, 3).equals("bad") || str.substring(1, 4).equals("bad"))){
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    atFirst
+    Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2,
+    use '@' for the missing chars.
+    */
+    public String atFirst(String str) {
+        if (str.length() < 1) {
+            return str + '@' + '@';
+        } else if (str.length() < 2) {
+            return str + '@';
+        } else {
+            return str.substring(0, 2);
+        }
+    }
+
+    /*
+    lastChars
+    Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo"
+    and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+    */
+    public String lastChars(String a, String b) {
+        String combStr = "";
+
+        combStr += (a.length() >= 1) ? a.charAt(0) : '@';
+        combStr += (b.length() >= 1) ? b.charAt(b.length() - 1) : '@';
+        return combStr;
+    }
+
+    /*
+    conCat
+    Given two strings, append them together (known as "concatenation") and return the result. However,
+    if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+    */
+    public String conCat(String a, String b) {
+        if (a.length() > 0 && b.length() > 0) {
+            if (a.charAt(a.length() - 1) == b.charAt(0)) {
+                return a + b.substring(1);
+            } else {
+                return a + b;
+            }
+        }
+        return a + b;
+    }
+
+    /*
+    lastTwo
+    Given a string of any length, return a new string where the last 2 chars, if present,
+    are swapped, so "coding" yields "codign".
+    */
+    public String lastTwo(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, str.length() - 2) + str.charAt(str.length() - 1) + str.charAt(str.length() -2);
+        } else {
+            return str;
+        }
+    }
+
+    /*
+    seeColor
+    Given a string, if the string begins with "red" or "blue" return that color string,
+    otherwise return the empty string.
+    */
+    public String seeColor(String str) {
+        if (str.length() >= 4) {
+            if (str.substring(0, 4).equals("blue")) {
+                return "blue";
+            } else if (str.substring(0, 3).equals("red")) {
+                return "red";
+            } else {
+                return "";
+            }
+        } else if (str.length() == 3) {
+            if (str.substring(0, 3).equals("red")) {
+                return "red";
+            } else {
+                return "";
+            }
+        }
+        return "";
+    }
+
+    /*
+    frontAgain
+    Given a string, return true if the first 2 chars in the string also appear at the end of the string,
+    such as with "edited".
+    */
+    public boolean frontAgain(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2).equals(str.substring(str.length() - 2));
+        }
+        return false;
+    }
+
+    /*
+
+    */
+
+
+
 
 }
